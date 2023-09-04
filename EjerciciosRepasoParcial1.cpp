70eb6425085065f7273e2d9e10a69ef8023a4368
Alexander Morales <70553543+Alex22sv@users.noreply.github.com>
2023-09-04 07:52:20 -0600

    Corregir errores en ejercicio de repaso para parcial 1

--- EjerciciosRepasoParcial1.cpp
+++ EjerciciosRepasoParcial1.cpp
@@ -14,6 +14,7 @@ struct BombaGasolinera {
     int cantGasolina;
     int cantDiesel;
 } bombaGasolinera;
+
 void LeerCambioBombaGasolinera(float &actual);
 void ActualizarBombaGasolinera(struct BombaGasolinera &bm, int opcion);
 
@@ -21,10 +22,10 @@ int main(){
     // Función que permite mostrar los caracteres especiales en la terminal
     SetConsoleOutputCP(CP_UTF8);
     int opcion;
-    cout<<"Bienvenido! \n¿Deseas llenar gasolina o diesel?\n1. Gasolina\2. Diésel\n...";
+    cout<<"Bienvenido! \n¿Deseas llenar gasolina o diesel?\n1. Gasolina\n2. Diésel\n...";
     cin>>opcion;
     while(true){
-        ActualizarBombaGasolinera(struct BombaGasolinera bombaGasolinera, int opcion);
+        ActualizarBombaGasolinera(bombaGasolinera, opcion);
         cout<<"Valores: \nGasolina: "<<bombaGasolinera.cantGasolina<<"\nDiésel: "<<bombaGasolinera.cantDiesel<<"\n";
         Sleep(2000);
     }
@@ -33,13 +34,13 @@ int main(){
 void LeerCambioBombaGasolinera(float &actual){
     int t = time(NULL);
     srand(t);
-    cout<<" - valor: "<<t;
+    //cout<<" - valor: "<<t;
     actual = rand() % (10+1);
 }
 void ActualizarBombaGasolinera(struct BombaGasolinera &bm, int opcion){
     float actual = 0.00;
     LeerCambioBombaGasolinera(actual);
-    cout<<"Lectura actual: "<<actual<<"\n";
+    //cout<<"Lectura actual: "<<actual<<"\n";
     if(opcion==1){
         bm.cantGasolina += actual;
     } else if(opcion==2){
