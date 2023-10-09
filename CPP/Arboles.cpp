0ad614e6dba2eec64cf0a005a397ddc01522ff60
Alexander Morales <70553543+Alex22sv@users.noreply.github.com>
2023-10-09 08:21:28 -0600

    Actualizar árboles con clase de hoy

--- CPP/Arboles.cpp
+++ CPP/Arboles.cpp
@@ -20,11 +20,12 @@ using namespace std;
         - degenerado (solo existe un nodo hoja y cada nodo no hoja tiene un solo hijo)
         - lleno (cada nodo tiene la misma cantidad de hijos)
         - completo (el árbol está lleno hasta el penúltimo nivel y el último tiene más nodos a la izquierda)
-    Tipos de recooridos: 
+    Tipos de recorridos: 
         - in orden (izquierda->raíz->derecha)
         - pre orden (raíz->izquierda->derecha)
         - post orden (izquierda->derecha->raíz)
         Los árboles no son grafo, porque no tienen ciclos.
+    Propiedades: grado, orden, nivel de un nodo, longitud.
 */
 
 struct Nodo {
@@ -38,6 +39,9 @@ struct Nodo *ABB = nullptr;
 
 struct Nodo *Crear(int);
 struct Nodo *Insertar(struct Nodo *, int);
+void Borrar(struct Nodo *&, int n);
+void Buscar(struct Nodo *&, int, struct Nodo *&);
+struct Nodo BuscarMenor(struct Nodo *);
 void PreOrden(struct Nodo *);
 void PostOrden(struct Nodo *);
 
@@ -63,6 +67,69 @@ struct Nodo *Insertar(struct Nodo *ABB, int n){
         ABB->izquierdo = Insertar(ABB->izquierdo, n);
     } else {
         ABB->derecho = Insertar(ABB->derecho, n);
+    }
+    return ABB;
+}
+void Borrar(Nodo *&ABB, int n){
+    struct Nodo *padre = nullptr;
+    // ABB nodo raíz
+    struct Nodo *aux = ABB;
+    Buscar(aux, n, padre);
+    //Si el árbol no tiene nodos;
+    if(aux==nullptr){
+        return;
+    }
+    // Borrar un nodo hoja
+    if((aux->izquierdo==nullptr) && (aux->derecho==nullptr)){
+        if(aux==ABB){
+            ABB = nullptr;
+        } else {
+            if(padre->izquierdo==nullptr){
+                padre->izquierdo = nullptr;
+            } else {
+                padre->derecho = nullptr;
+            }
+        }
+        delete aux;
+    } else if ((aux->izquierdo) && (aux->derecho)){
+        // Borrar nodo con 2 hijos (hijos en der. e izq.)
+        //Buscamos en el subárbol derecho el nodo menor
+        struct Nodo *sucesor = BuscarMenor(aux->derecho);
+        int val = sucesor->dato;
+        Borrar(ABB, sucesor->dato);
+        aux->dato = val;
+    } else {
+        // Borrar un nodo con un solo hijo
+        struct Nodo *hijo;
+        if(aux->izquierdo){
+            hijo = aux->izquierdo;
+        } else {
+            hijo = aux->derecho;
+        }
+        if(aux!=ABB){
+            if(aux==padre->izquierdo){
+                padre->izquierdo = hijo;
+            } else  {
+                padre->derecho = hijo;
+            }
+        } else {
+            ABB = hijo;
+        }
+    }
+}
+void Buscar( struct Nodo *&ABB, int n, struct Nodo *&padre){
+    while((ABB!=nullptr) && (ABB->dato!=n)){
+        padre = ABB;
+        if(n<ABB->dato){
+            ABB = ABB->izquierdo;
+        } else {
+            ABB = ABB->derecho;
+        }
+    }
+}
+struct Nodo *BuscarMenor(struct nodo *ABB){
+    while(ABB!=nullptr){
+        ABB = ABB->izquierdo;
     }
     return ABB;
 }
